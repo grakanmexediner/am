a8cf7124be40c717a8659483716a1e0ad624e1c0
grakanmexediner <77056454+grakanmexediner@users.noreply.github.com>
2021-01-06 16:26:15 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function business(){
+				alert("Error")
+}
+function addres(){
+				alert("Error")
+}
+function webemail(){
+				alert("Error")
+}
+function cloudflare(){
+				var name = prompt("Enter Your Name")
+}
